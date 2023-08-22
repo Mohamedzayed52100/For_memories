bfda07685f1229a4baf4ee61f43972d4a56b7d33
=Mohamed Zayed <mohamedzayed52100@gmail.com>
2023-08-23 02:24:06 +0300

    a

--- namespace.php
+++ namespace.php
@@ -5,4 +5,52 @@
 
 
 
+طريقة لتجيمع ملفات ال php
+
+name collections حل مشكلة 
+
+اللي هي تضارب الاسماء بمعني لو عندي ملفين بنفسم الاسم او 2 كلاس بنفس الاسم 
+
+
+التنظيم 
+
+
+autoloading علشان مكتبش ال path 
+علشان مكتبش 
+require and include 
+
 */
+
+interface aA{
+      function test();
+
+}
+
+
+abstract class TTP{
+    private $g =10 ;
+   abstract protected function TR();
+
+
+   public static function sayName(){
+    echo "mohamed";
+   }
+}
+
+class BD implements aA{
+    function test(){
+        echo "test";
+    }
+}
+
+
+$N = new BD();
+$N->test();
+
+
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+TTP::sayName();
