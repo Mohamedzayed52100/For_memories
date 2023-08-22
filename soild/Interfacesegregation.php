c0abcae37748e9fe7d399a880639c5a4259a63d7
=Mohamed Zayed <mohamedzayed52100@gmail.com>
2023-08-22 22:22:02 +0300

    correct

--- soild/Interfacesegregation.php
+++ soild/Interfacesegregation.php
@@ -6,3 +6,56 @@
 اللي هتعمله imolement مش تعمل implemnt لكل الفانكشن 
 
 */
+
+
+interface QuackableInterface{
+    public function Quack();
+}
+interface FlyableInterface{
+    public function Fly();
+}
+interface swimmableInterface{
+    public function swim();
+}
+
+
+/*
+
+لو عندي نوع من البط بيسبح ويجري 
+
+
+
+*/
+
+class swim_and_quack_brid implements QuackableInterface , swimmableInterface{
+    public function Quack(){
+        echo "Quack";
+
+    }
+    public function swim(){
+        return 'swim';
+
+    }
+}
+
+
+/*
+
+لو عندي نوع من البط بيسبح وبيطير   
+
+
+
+*/
+
+
+class swim_and_fly_brid implements FlyableInterface , swimmableInterface{
+    public function Fly(){
+        echo "swim";
+
+    }
+    public function swim(){
+        return 'swim';
+
+    }
+}
+
--- soild/Liskovsubstitution.php
+++ soild/Liskovsubstitution.php
@@ -22,57 +22,46 @@ A obj =new C();
 لو الاب مش موجود الابن يقدر يحل مكان الاب 
 
 
-https://mohasin-dev.medium.com/how-to-use-liskov-substitution-principle-in-php-laravel-df253fd38c8
-*/
-
-interface QuackableInterface{
-    public function Quack();
-}
-interface FlyableInterface{
-    public function Fly();
-}
-interface swimmableInterface{
-    public function swim();
-}
-
-
-/*
-
-لو عندي نوع من البط بيسبح ويجري 
-
-
-
-*/
-
-class swim_and_quack_brid implements QuackableInterface , swimmableInterface{
-    public function Quack(){
-        echo "Quack";
-
-    }
-    public function swim(){
-        return 'swim';
-
-    }
-}
-
-
-/*
-
-لو عندي نوع من البط بيسبح وبيطير   
-
-
-
 */
 
 
-class swim_and_fly_brid implements FlyableInterface , swimmableInterface{
-    public function Fly(){
-        echo "swim";
 
+////////////////////////////////////////////////////
+
+
+class TheParent {
+    public function DoWork(){
+        echo "this is paraent class and DoWork function";
     }
-    public function swim(){
-        return 'swim';
+    
+}
 
+
+class Child extends TheParent{
+    public function DoWork(){
+        echo "this is Child class and DoWork function";
     }
 }
+
+
+function XYZ(TheParent $theParent){
+    $theParent->DoWork();
+
+}
+
+
+
+
+$newObj =new TheParent();
+
+
+XYZ($newObj);
+
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+$newObj = new Child();
+XYZ($newObj);
 
